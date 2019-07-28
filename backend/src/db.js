4827cc224589f9e69fd5f529564d8410379bf33d
Fran Zekan <zekan.fran369@gmail.com>
2019-07-28 15:02:53 +0200

    Lesson 16 GQL init

---
+++ backend/src/db.js
@@ -0,0 +1,12 @@
+// Connects to remote prisma db and sets up querying
+
+const { Prisma } = require("prisma-binding");
+
+const db = new Prisma({
+  typeDefs: "src/generated/prisma.graphql",
+  endpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_SECRET,
+  debug: true
+});
+
+module.exports = db;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,21 @@
-// let's go!
+require("dotenv").config({ path: "variables.env" });
+
+const createServer = require("./createServer");
+const db = require("./db");
+
+const server = createServer();
+
+// TODO User express midddleware for cookies (JWT)
+// TODO User express midddleware for current user
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FROTEND_URL
+    }
+  },
+  data => {
+    console.log(`Server is now running http://localhost:${data.port}`);
+  }
+);
