27bb3c67dd4c860379e0a36d9aabb8916afc359e
Fran Zekan <zekan.fran369@gmail.com>
2019-07-28 12:59:38 +0200

    Lesson 10 finished

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,17 +1,80 @@
 import React from "react";
+import Link from "next/link";
+import Router from "next/router";
+import styled, { consolidateStreamedStyles } from "styled-components";
+import NProgress from "nprogress";
+
 import Nav from "./Nav";
+
+Router.onRouteChangeStart = () => {
+  NProgress.start();
+};
+
+Router.onRouteChangeComplete = () => {
+  NProgress.done();
+};
+
+Router.onRouteChangeError = () => {
+  NProgress.done();
+};
+
+const Logo = styled.h1`
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  transform: skew(-7deg);
+
+  a {
+    padding: 0.5rem 1rem;
+    background: ${props => props.theme.red};
+    color: white;
+    text-transform: uppercase;
+    text-decoration: none;
+  }
+
+  @media (max-width: 1300px) {
+    margin: 0;
+    text-align: center;
+  }
+`;
+
+const StyledHeader = styled.header`
+  .bar {
+    border-bottom: 10px solid ${props => props.theme.back};
+    display: grid;
+    justify-content: space-between;
+    align-items: stretch;
+
+    @media (max-width: 1300px) {
+      grid-template-columns: 1fr;
+      justify-content: center;
+    }
+  }
+
+  .sub-bar {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 1px solid ${props => props.theme.lightGrey};
+  }
+`;
 
 const Header = () => {
   return (
-    <div>
+    <StyledHeader>
       <div className="bar">
+        <Logo>
+          <Link href="/">
+            <a>Sick fits</a>
+          </Link>
+        </Logo>
         <Nav />
       </div>
       <div className="sub-bar">
         <p>Serach</p>
       </div>
       <div>Cart</div>
-    </div>
+    </StyledHeader>
   );
 };
 
